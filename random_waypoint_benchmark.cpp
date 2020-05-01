034b98c30e1f6ec13317df43ea24ec8015fd9f78
chris-j-tang <chrisjtang98@gmail.com>
2020-04-30 21:58:31 -0400

    fxn prototypes path and point struct

--- random_waypoint_benchmark.cpp
+++ random_waypoint_benchmark.cpp
@@ -40,29 +40,43 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
-#define NUM_ARGS 13
-// TODO create paths
-// std::map<size_t > 
+#define RAND_SEED 52021
+#define MAP_SIZE 10
+#define NUM_TRXS 4
+#define MAX_BUFFER_SIZE 2048   // bytes
+#define SIMULATION_DURATION 15 // secs
+#define TIME_STEP 5          // secs
+#define PERIOD 
+#define COMM_RANGE
+#define SEND_DELAY 20    // ms
+#define RECV_DELAY 30    // ms
+#define SPEED_MAX 
+#define SPEED_MIN
+
+// Singular component making up a trx's path.
+typedef struct Point {
+    double curr_x, curr_y;    // New location of a trx
+    double curr_time;         // at time curr_time.
+} Point;
+
+/**
+ * Plots the random waypoint path a trx travels.
+ * 
+ * Args:
+ *      rank: Current rank, used so trxs with same IDs across ranks don't
+ *            have the same path defined.
+ *      id: Unique m_id of the trx whose path is calculated.
+ * Returns:
+ *      The collection of Points in chronological order the trx specified by
+ *      the args follows.
+ */
+ std::queue<Point> plot_path(int rank, size_t id) {
+     std::queue<Point> path;
+     
+     return path;
+ }
 
 int main(int argc, char** argv) {
-    if (argc != NUM_ARGS) {
-        cout << 
-        "Usage: \
-        ./run.exe <random-seed> <map-size> <# of trxs/rank> <buffer size/trx> \
-        <simulation duration in ms> <time-step to adjust velocity (ms)> \
-        <period of transmitted data> <communication range/each trx> \
-        <send time delay/trx (ms)> <rcv time delay/trx (ms)> \
-        <lower bound on trx move speed (m/s)> \
-        <upper bound on trx move speed (m/s)>" << endl;
-        return 0;
-    }
-    // User's responsibility to send in right types.
-    static const float random_seed = atoi(argv[1]);    
-    static const size_t map_size = atoi(argv[2]);
-    static const size_t num_trxs = atoi(argv[3]); 
-    static const size_t max_buffer_size = atoi(argv[4]);
-    static const double simulation_duration = atoi(argv[5]);
-
     // Initializes MPI environment.
     int prov;
     int ret = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &prov);
@@ -83,20 +97,29 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    auto trxs = MPIRadioTransceiver<max_buffer_size>::transceivers<num_trxs>();
+    auto trxs = MPIRadioTransceiver<MAX_BUFFER_SIZE>::transceivers<NUM_TRXS>();
     if (trxs == nullptr) { // Unable to retrieve transceivers.
         MPI_Finalize();
         return 1;
     }
 
-    // Create paths for transceiver moves at a certain point in time using the
-    // random waypoint pattern.
+    srand(RAND_SEED);
+    // Transceiver and path initialization.
+    std::map<size_t, std::queue<Point>> trx_to_path;
+    for (size_t i = 0; i < NUM_TRXS; ++i) {
+        auto& t = trxs[i];
+        // Create paths for transceiver moves at a certain point in time using
+        // the random waypoint pattern.
+    }
 
-    // Transceiver initialization.
-    //
+    /**
+    * This test places all the transceivers on a finitely-sized world, and
+    * and runs for a speciied duration. At intervals of a pre-defined time
+    * step, every transceiver moves following the random waypoint pattern.
+    */  
 
     // Shuts down all transceivers.
-    MPIRadioTransceiver<max_buffer_size>::close_transceivers(num_trxs>(trxs);
+    MPIRadioTransceiver<MAX_BUFFER_SIZE>::close_transceivers<NUM_TRXS>(trxs);
 
     // Synchronize MPI ranks.
     MPI_Finalize();
