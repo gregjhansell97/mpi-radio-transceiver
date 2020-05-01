f321969714fa09b64355dcbc83963f2a39b87573
chris-j-tang <chrisjtang98@gmail.com>
2020-05-01 01:33:40 -0400

    queue -> deque and trxs move

--- random_waypoint_benchmark.cpp
+++ random_waypoint_benchmark.cpp
@@ -31,17 +31,17 @@ using std::cerr;
 using std::endl;
 
 #define RAND_SEED 52021
-#define MAP_SIZE 10 // Square world with no wrap-around
+#define MAP_SIZE 1000 // Square world with no wrap-around.
 #define NUM_TRXS 4
 #define MAX_BUFFER_SIZE 2048   // bytes
-#define SIMULATION_DURATION 15 // secs
-#define TIME_STEP 5          // secs
-#define PERIOD 
-#define COMM_RANGE 2     // send and recv range equal TODO do they have to be equal?
-#define SEND_DELAY 20    // ms
-#define RECV_DELAY 30    // ms
-#define SPEED_MAX 3
-#define SPEED_MIN 1
+#define SIMULATION_DURATION 15 // iteration count
+#define TIME_STEP 3          // interval between transceiver moves
+#define DATA_PERIOD 2         // Intervals of data transmission.
+#define COMM_RANGE 125     // send and recv ranges.
+#define SEND_DELAY 0.01    // ms
+#define RECV_DELAY 0.001    // ms
+#define SPEED_MAX 20
+#define SPEED_MIN 10
 
 // Singular component making up a trx's path.
 typedef struct Point {
@@ -57,12 +57,12 @@ typedef struct Point {
  * Returns:
  *      Collection of Points in chronological order.
  */
- std::queue<Point> plot_path(std::mt19937 gen) {
-    std::queue<Point> path; // Collection of Points dictating trx id's path.
+ std::deque<Point> plot_path(std::mt19937 gen) {
+    std::deque<Point> path; // Collection of Points dictating trx id's path.
     // Transforms gen's random uint into a double in [0, MAP_SIZE).
     std::uniform_real_distribution<> dis_loc(0, MAP_SIZE);
     Point point = {dis_loc(gen), dis_loc(gen), 0};
-    path.push(point);
+    path.push_back(point);
 
     // Transforms gen's random uint into a double in [SPEED_MIN, SPEED_MAX).
     std::uniform_real_distribution<> dis_sp(SPEED_MIN, SPEED_MAX);
@@ -73,10 +73,11 @@ typedef struct Point {
         const Point prev = path.back();
         double next_x = dis_loc(gen);
         double next_y = dis_loc(gen);
-        double dist = sqrt(pow((prev.x - next_x), 2) + pow((prev.y - next_y), 2));
+        double dist =
+            sqrt(pow((prev.x - next_x), 2) + pow((prev.y - next_y), 2));
         double next_time = dist/speed + prev.time;
         Point next = {next_x, next_y, next_time};
-        path.push(next);
+        path.push_back(next);
     }
     return path;
  }
@@ -110,7 +111,7 @@ int main(int argc, char** argv) {
 
     std::mt19937 gen(RAND_SEED);
     // Transceiver and path initialization.
-    std::map<size_t, std::queue<Point>> trx_to_path;
+    std::map<size_t, std::deque<Point>> trx_to_path;
     for (size_t i = 0; i < NUM_TRXS; ++i) {
         // Create paths for transceiver moves at a certain point in time using
         // the random waypoint pattern.
@@ -129,14 +130,55 @@ int main(int argc, char** argv) {
 
     /**
     * This test places all the transceivers on a finitely-sized world, and
-    * and runs for a speciied duration. At intervals of a pre-defined time
+    * and runs for a specified duration. At intervals of a pre-defined time
     * step, every transceiver moves following the random waypoint pattern.
     * 
-    * 
+    * A subset of transceivers periodically transmit data. This data is the
+    * time of transmission. The receivers (which are not the senders, and
+    * waiting on separate threads) that are within communication range record
+    * the latency for further performance calculation and measures later.
     */
+    int start_time; // Simulation start time.
+    int op_time = 0; // Time spent on operations, subtracted off.
+    int curr_time; // Current time spent in simulation.
+    start_time = MPI_Wtime();
+    // Total elapsed time disregarding time spent on operations.
+    while ( (curr_time = MPI_Wtime() - start_time - op_time)
+        < SIMULATION_DURATION) {
+        // Keep track of elapsed time in single operation.
+        double op_start = MPI_Wtime();
+        // Check if trxs are moving this time interval
+        if (curr_time % TIME_STEP == 0) { // All trxs are moving!
+            for (size_t i = 0; i < NUM_TRXS; ++i) {
+                std::deque<Point> path = trx_to_path.at(i);
+                Point p1 = path[1];
+                while (curr_time > p1.time) { // Pop off Points until curr_time
+                    path.pop_front();
+                    p1 = path[1];
+                }
+                Point p0 = path.front();
+                double offset = (double)(curr_time - p0.time)/(p1.time - p0.time);
+                double x = p0.x + offset * (p1.x - p0.x);
+                double y = p0.y + offset * (p1.y - p0.y);
+                // Access trx corresponding with this move and set new loc
+                auto& t = trxs[i];
+                t.set_x(x);
+                t.set_y(y);
+            }
+        }
+        // Check if trxs are sending out msgs this time interval
+        if (curr_time % DATA_PERIOD == 0) { // trxs 0 and 1 send out curr time
+            
+        }
+
+        double op_end = MPI_Wtime(); // Take end time of operations
+        op_time += op_end - op_start; // op_time will miss this (albeit small)
+    }
+
     // TODO accummulate sums and avg across # of transceivers
     double sum_of_all_latencies = 0;
-
+    // TODO min/max-heap find median from stream of data
+    // TODO create arr of latency sums[NUM_TRXS] and msg_counter per rank
     /**
      * Close transceivers and sync information across ranks.
      * Calculate average, median, and std dev latencies across all trxs in all
